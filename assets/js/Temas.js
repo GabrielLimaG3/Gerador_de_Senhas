f0d4847c07a071483ee2617685fe1c2b331a21c0
Gabriel <ga.lima2205@gmail.com>
2023-12-25 14:18:41 -0300

    update tema

--- assets/js/Temas.js
+++ assets/js/Temas.js
@@ -1,5 +1,7 @@
 var btn = document.getElementsByClassName('btn')[0]
 var btn_gerar =document.querySelector('.btn_gerar_light')
+let text_senha = document.querySelector('.senha')
+let conteiner_senha = document.querySelector('.conteiner_senha')
 
 
 btn.addEventListener('click', mudartema)
@@ -10,6 +12,12 @@ function mudartema(){
 
     btn.classList.toggle('btn_white')
     btn_gerar.classList.toggle('btn_gerar_dark')
+
+    if(text_senha != text_senha.classList.toggle('senha')){
+        text_senha.classList.toggle('senha-light')
+    }{
+        conteiner_senha.classList.toggle('conteiner_senha_light')
+    }
 
     if(btn.innerHTML == 'light'){
         btn.innerHTML = 'dark'
